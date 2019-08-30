65f2450be4b12ec003337e4f0e55123864b2e132
Scott Vandehey <fake-scott@spaceninja.com>
2019-08-30 15:56:02 -0700

    09 vuex mutation history and vue devtools

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -4,7 +4,7 @@
     <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" alt="Loading…" />
     <ul v-else>
       <li v-for="product in availableProducts" :key="product.id">
-        {{ product.title }} - {{ product.price }}
+        {{ product.title }} - {{ product.price }} - {{ product.inventory }}
         <button @click="addProductToCart(product)">Add to cart</button>
       </li>
     </ul>
